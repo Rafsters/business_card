9401faa3c470afbd1dba4fa677ccc7fb1177c27c
Rafal Kwiecien <Rafsters@interia.pl>
2018-03-12 00:43:11 +0100

    Motion detection

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -41,3 +41,18 @@ function checkConnection() {
 
     alert('Connection type: ' + states[networkState]);
 }
+
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
+
+function checkPosition(){
+	navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+}
